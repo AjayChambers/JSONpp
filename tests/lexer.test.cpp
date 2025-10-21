ab172a250d8fc90d3f94d67960e85f4bdfd1680a
Andrew Chambers <w3dojo@gmail.com>
2025-10-20 20:45:18 -0700

    added lexer.test.cpp for lexer_test

---
+++ tests/lexer.test.cpp
@@ -0,0 +1,16 @@
+
+
+#include <json/lexer.hpp>
+#include <gtest/gtest.h>
+
+#include <format>
+#include <string>
+#include <string_view>
+
+using namespace Json;
+
+namespace Json {
+
+// LexResult_T JsonLexer::lexString(){}
+
+}
